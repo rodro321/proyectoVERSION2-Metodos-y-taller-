ddda9fc49d5848eea366078b3562125f8716c1b0
Rodrigo Alejandro Choque Bautista <131217401+rodro321@users.noreply.github.com>
2024-11-11 09:08:40 -0400

    Add files via upload

---
+++ BuscaMinasConsola/BuscaMinas/BuscaMinas/src/main/java/rodrocompany/model/BuscaMinas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package rodrocompany.model;
+
+/**
+ *
+ * @author win
+ */
+public class BuscaMinas {
+
+    public static void main(String[] args) {
+        System.out.println("BUSCA MINAS");
+
+               
+        Modelo model = new Modelo();
+        model.iniciarJuego();
+
+    }
+
+}
